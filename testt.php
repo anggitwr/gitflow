6ab232d0a2627abe2bbb3d272c52a150e3c8dd3e
anggittwr <agtwahyu23@gmail.com>
2023-11-18 22:10:08 +0700

    commit tes branch pengiriman

---
+++ testt.php
@@ -0,0 +1,102 @@
+<?php
+
+// function tambah($a, $b){
+//     return $a + $b;
+// }
+
+// echo tambah(5,5);
+// echo "hello";
+
+// $nama = "anggit";
+// echo ("namanya adalah : ");
+// echo ($nama);
+
+// $a = [
+//     "first_name" => "eko",
+//     "last_name" => "khan"
+// ];
+
+// var_dump("tessst");
+// var_dump($a);
+// function getArray()
+// {
+//     return array(1, 2, 3);
+// }
+
+// $second = getArray(2);
+
+// var_dump($second);
+
+// for ($i = 0; $i <= 10; $i++) {
+//     for ($j = 0; $j <= $i; $j++) {
+//         echo "*";
+//     }
+// }
+
+// function segitigaBintang($sisi)
+// {
+//     for ($i = 0; $i < $sisi; $i++) {
+//         for ($j = 0; $j <= $i; $j++) {
+//             echo "*";
+//         }
+//         echo "\n";
+//     }
+// }
+
+// segitigaBintang(5);
+
+// for ($i = 0; $i < 5; $i++) {
+//     for ($j = 0; $j <= $i; $j++) {
+//         echo "*";
+//     }
+//     echo "\n";
+// }
+
+// for ($i = 1;; $i++) {
+//     if ($i > 10) {
+//         break;
+//     }
+//     echo $i;
+//     $i++;
+// }
+
+// for ($i = 1;; $i++) {
+//     if ($i > 10) {
+//         break;
+//     }
+//     echo $i;
+// }
+
+// $arr = array('one', 'two', 'three');
+
+// foreach ($arr as $val) {
+//     if ($val == 'two') {
+//         break;
+//     }
+//     echo "$val \n";
+// }
+
+
+// $arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six'];
+
+// foreach ($arr as $key => $value) {
+//     if (0 === ($key % 2)) {
+//         continue;
+//     }
+//     echo $value . "\n";
+// }
+
+
+// $i = 1;
+
+// switch ($i) {
+//     case 0:
+//         echo "i equals 0";
+//         break;
+//     case 1:
+//         echo "i equals 1";
+//         break;
+//     case 2:
+//         echo "i equals 2";
+//         break;
+// }
